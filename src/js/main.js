eb6dff605c67623af2d07d9b1b95643a1d678ad3
anabescos <anabes88@gmail.com>
2021-01-25 20:31:45 +0100

    Add functionality to the form

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,19 @@ function handleFullName (event) {
 
 fullNameElement.addEventListener ('keyup', handleFullName);
 
+const emailElement = document.querySelector ('.js-email');
+const emailGenerator = document.querySelector('.js-generatorEmail');
 
+function handleEmail (event){
+    let email = event.target.value;
+    console.log('Hola');
+    if (email === ""){
+        console.log('kjsdfla');
+        emailGenerator.innerHTML = "";
+    }
+    else {
+    emailGenerator.href = `mailto:${email}`;
+    console.log('Holaaaaaaaa');
+    }
+}
+emailElement.addEventListener('keyup', handleEmail);
