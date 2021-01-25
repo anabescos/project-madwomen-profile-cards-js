fe1287268d0bbe3951d3bb16c97b2d545e8066ee
anabescos <anabes88@gmail.com>
2021-01-25 19:38:58 +0100

    Add functionality to form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+const fullNameElement = document.querySelector('.js-fullName');
+const fullNameGenerator = document.querySelector ('.js-generatorName');
+
+function handleFullName (event) {
+    
+    const name = event.target.value;
+    
+    if (name === ""){
+        
+        fullNameGenerator.innerHTML = "Nombre Apellido";
+    }
+    else {
+    fullNameGenerator.innerHTML = name;
+    }
+}
+
+fullNameElement.addEventListener ('keyup', handleFullName);
+
+
