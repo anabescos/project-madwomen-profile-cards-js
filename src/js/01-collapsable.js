ef746ecd1e33fd1bfeab9415c52ceccc7fcde37c
Victoria Kovaleva <victoriak.dev@gmail.com>
2021-01-31 10:46:29 +0100

    Add part of JavaScript for arrows

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,12 +1,27 @@
 
 'use strict';
 
-const titleAreaElement = document.querySelector('.js-title');
-const containerElement = document.querySelector('.js-container');
+// const titleAreaElement = document.querySelector('.js-title');
+// const containerElement = document.querySelector('.js-container');
 
-function handleCollapsable (event){
-    containerElement.classList.toggle('collapsable__close');
+// function handleCollapsable (event){
+//     containerElement.classList.toggle('collapsable__close');
+// }
+
+
+// titleAreaElement.addEventListener ('click', handleCollapsable);
+
+
+const titleAreaElement = document.querySelectorAll('.js-title');
+const containerElement = document.querySelectorAll('.js-container');
+
+function handleCollapsable (){
+    for (let i = 0; i < containerElement.length; i++) {
+    containerElement[i].classList.toggle('collapsable__close');
+    }
 }
 
 
-titleAreaElement.addEventListener ('click', handleCollapsable);
+titleAreaElement[0].addEventListener ('click', handleCollapsable);
+titleAreaElement[1].addEventListener ('click', handleCollapsable);
+titleAreaElement[2].addEventListener ('click', handleCollapsable);
