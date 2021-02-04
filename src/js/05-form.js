8126dd79e8eefa1873fca9bed3cab9c407b83344
anabescos <anabes88@gmail.com>
2021-02-04 14:07:09 +0100

    Add js partials and js code to save in localStorage

---
+++ src/js/05-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const submitElement = document.querySelector('.js-submit');
+
+function handleSubmit(ev) {
+    ev.preventDefault();
+}
+
+submitElement.addEventListener('submit', handleSubmit);
